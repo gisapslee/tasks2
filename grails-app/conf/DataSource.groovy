5858a975b529c0f6127c6ecc15f80022dcbb57d8
gisapslee <gisapslee@gmail.com>
2015-08-11 00:19:12 -0300

    alteração datasource 3

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,11 +1,10 @@
 dataSource {
     pooled = true
     jmxExport = true
-    driverClassName = "org.postgresql.Driver"
-    username = "gqozrqawfflcxe"
-    password = "_QRyqJ8tLRUolG5DWNvQiLBEMc"    
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
 }
-
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -19,30 +18,20 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" 
-            url = "jdbc:postgresql:postgres"            
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:postgresql:postgres"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQLDialect
-
-            uri = new URI(System.env.DATABASE_URL?:"postgres://gqozrqawfflcxe:_QRyqJ8tLRUolG5DWNvQiLBEMc@ec2-54-204-3-200.compute-1.amazonaws.com:5432/d6cpk7bjbdv92")
-
-            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-            username = uri.userInfo.split(":")[0]
-            password = uri.userInfo.split(":")[1]
-            dbCreate = "update"
-            url = "jdbc:postgresql:postgres"
-
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
